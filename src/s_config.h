c3cef70c3709cbcda8bf54742edb08af35fb3a19
Dennis ten Hoove <dennistenhoove@protonmail.com>
2023-10-25 20:47:23 +0000

    Set min IDs to 0

--- src/s_config.h
+++ src/s_config.h
@@ -1,6 +1,6 @@
 #define USERSFILE "/usr/lib/passwd"
 #define SHADOWFILE "/usr/lib/shadow"
 #define GROUPSFILE "/usr/lib/group"
-#define MINUID 500
-#define MINGID 500
-#define USERSGID 100
+#define MINUID 0
+#define MINGID 0
+#define USERSGID 0
